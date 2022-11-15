5d0e5d2d0a4706c6304688c6c31415dc4785926f
zhangjingqing007 <515749390@qq.com>
2022-11-15 09:37:54 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-kqgmps
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-kqgmps)
---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- <div>{{ runtimeConfig }}</div> -->
+
+    <div>{{ appConfig }}</div>
+
+    <NuxtWelcome />
+  </div>
+</template>
+<script setup>
+const runtimeConfig = useRuntimeConfig();
+const appConfig = useAppConfig();
+</script>
